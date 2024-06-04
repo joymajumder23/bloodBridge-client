bd554db800968f05bd7d3d3c86193e9caf6d4799
joymajumder23 <joymajumderjishu@gmail.com>
2024-06-05 03:38:33 +0600

    create donation request adding

--- src/components/Dashboard/CreateDonation/CreateDonation.jsx
+++ src/components/Dashboard/CreateDonation/CreateDonation.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import TimePicker from 'react-time-picker';
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import useAuth from "../../Hooks/useAuth";
 
 const CreateDonation = () => {
     const [districts, setDistricts] = useState([]);
     const [upazilas, setUpazilas] = useState([]);
-    useEffect(() => {
-        fetch('./districts.json')
-            .then(res => res.json())
-            .then(data => setDistricts(data))
-    }, [])
+    const [value, onChange] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
 
     useEffect(() => {
-        fetch('./upazilas.json')
+        fetch('./../districts.json')
+            .then(res => res.json())
+            .then(data => setDistricts(data))
+    }, []);
+    console.log(districts);
+
+    useEffect(() => {
+        fetch('./../upazilas.json')
             .then(res => res.json())
             .then(data => setUpazilas(data))
     }, []);
 
+    const { user } = useAuth();
 
     const {
         register,
@@ -24,70 +35,130 @@ const CreateDonation = () => {
     } = useForm();
 
     const onSubmit = (data) => {
-        console.log(data)
+        console.log(data);
+        const donationUser = {
+            requesterName: user?.displayName,
+            requesterEmail: user?.email,
+            recipientName: data.recipientName,
+            address: data.address,
+            blood: data.blood,
+            district: data.district,
+            upazila: data.upazila,
+            donationDate: startDate,
+            donationTime: value
+        };
+        console.log(donationUser);
     }
     return (
         <div>
+            <h1 className="text-3xl font-bold p-4 text-red-500">Create Donation Request</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="card-body w-full border">
-                <div className="form-control w-full">
-                    <label className="label">
-                        <span className="label-text">Recipient Name</span>
-                    </label>
-                    <input type="text" placeholder="name" className="input input-bordered w-full" name="name"  {...register("name")} required />
-                    {errors.name && <span>This field is required</span>}
-                </div>
-                <div className="form-control w-full">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input type="email" placeholder="email" className="input input-bordered" name="email" {...register("email")} required />
-                    {errors.email && <span>This field is required</span>}
-                </div>
+                <div className="flex gap-5">
+                    <div className="w-1/2">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Requester Name</span>
+                            </label>
+                            <input type="text" defaultValue={user?.displayName} className="input input-bordered max-w-xs" />
+                        </div>
 
-                <div className="form-control w-full">
-                    <label className="label">
-                        <span className="label-text">Blood Group</span>
-                    </label>
-                    <select className="select select-bordered w-full max-w-xs" {...register("blood")}>
-                        <option disabled selected>Select Blood Group</option>
-                        <option value="A+">A+</option>
-                        <option value="A-">A-</option>
-                        <option value="AB+">AB+</option>
-                        <option value="AB-">AB-</option>
-                        <option value="B+">B+</option>
-                        <option value="B-">B-</option>
-                        <option value="O+">O+</option>
-                        <option value="O-">O-</option>
-                    </select>
-                </div>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Recipient Name</span>
+                            </label>
+                            <input type="text" placeholder="name" className="input input-bordered w-full max-w-xs" name="recipientName"  {...register("recipientName")} required />
+                            {errors.name && <span>This field is required</span>}
+                        </div>
 
-                <div className="flex gap-4">
-                    <div className="form-control w-full">
-                        <label className="label">
-                            <span className="label-text">Recipient District</span>
-                        </label>
-                        <select className="select select-bordered w-full max-w-xs" {...register("district")}>
-                            <option disabled selected>Select District</option>
-                            {
-                                districts.map(district => <option key={district._id}  value={district.name}>{district.name}</option>)
-                            }
-                        </select>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Recipient District</span>
+                            </label>
+                            <select className="select select-bordered w-full max-w-xs" {...register("district")}>
+                                <option disabled selected>Select District</option>
+                                {
+                                    districts.map(district => <option key={district._id} value={district.name}>{district.name}</option>)
+                                }
+                            </select>
+                        </div>
+
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Blood Group</span>
+                            </label>
+                            <select className="select select-bordered w-full max-w-xs" {...register("blood")}>
+                                <option disabled selected>Select Blood Group</option>
+                                <option value="A+">A+</option>
+                                <option value="A-">A-</option>
+                                <option value="AB+">AB+</option>
+                                <option value="AB-">AB-</option>
+                                <option value="B+">B+</option>
+                                <option value="B-">B-</option>
+                                <option value="O+">O+</option>
+                                <option value="O-">O-</option>
+                            </select>
+                        </div>
+
+                        <div className="flex">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Donation Date</span>
+                                </label>
+                                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Donation Time</span>
+                                </label>
+                                <TimePicker onChange={onChange} value={value} />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="form-control w-full">
-                        <label className="label">
-                            <span className="label-text">Recipient Upazila</span>
-                        </label>
-                        <select className="select select-bordered w-full max-w-xs" {...register("upazila")}>
-                            <option disabled selected>Select Upazila</option>
-                            {
-                                upazilas.map(upazila => <option key={upazila._id} value={upazila.name}>{upazila.name}</option>)
-                            }
-                        </select>
+                    <div className="w-1/2">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Requester Email</span>
+                            </label>
+                            <input type="email" defaultValue={user?.email} className="input input-bordered max-w-xs" />
+                        </div>
+
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Address</span>
+                            </label>
+                            <input type="text" placeholder="address" className="input input-bordered max-w-xs" name="address"  {...register("address")} required />
+                            {errors.name && <span>This field is required</span>}
+                        </div>
+
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Recipient Upazila</span>
+                            </label>
+                            <select className="select select-bordered w-full max-w-xs" {...register("upazila")}>
+                                <option disabled selected>Select Upazila</option>
+                                {
+                                    upazilas.map(upazila => <option key={upazila._id} value={upazila.name}>{upazila.name}</option>)
+                                }
+                            </select>
+                        </div>
+
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Hospital Name</span>
+                            </label>
+                            <input type="text" placeholder="hospital name" className="input input-bordered max-w-xs" name="hospital"  {...register("hospital")} required />
+                            {errors.name && <span>This field is required</span>}
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Request Message</span>
+                            </label>
+                            <textarea className="textarea textarea-bordered w-full" placeholder="message"></textarea>
+                        </div>
                     </div>
-
                 </div>
-
                 <div>
                     <input className="btn rounded-none bg-red-600 text-white w-full" type="submit" value="Register" />
                 </div>
--- src/components/Layouts/Dashboard/Dashboard.jsx
+++ src/components/Layouts/Dashboard/Dashboard.jsx
@@ -1,31 +1,20 @@
 import { RiMenu2Line } from "react-icons/ri";
 import { NavLink, Outlet } from "react-router-dom";
+import Sidebar from "../../Dashboard/Sidebar/Sidebar";
 
 const Dashboard = () => {
     return (
-        <div>
-            <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center">
-                    {/* Page content here */}
-                    <div>
-                    <Outlet></Outlet>
-                    </div>
-                    <label htmlFor="my-drawer-2" className="btn drawer-button lg:hidden"><RiMenu2Line></RiMenu2Line></label>
-
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                        {/* Sidebar content here */}
-                        <li><NavLink to="/dashboard/donorsHome">Donor Home</NavLink></li>
-                        <li><NavLink to="/dashboard/profile">Profile</NavLink></li>
-                        <li><NavLink to="/dashboard/create-donation-request">Create Donation Request</NavLink></li>
-                    </ul>
-
-                </div>
-            </div>
+        <div className='relative min-h-screen md:flex'>
+        {/* Sidebar */}
+        <Sidebar />
+  
+        {/* Outlet --> Dynamic content */}
+        <div className='flex-1 md:ml-64'>
+          <div className='p-5'>
+            <Outlet />
+          </div>
         </div>
+      </div>
     );
 };
 
