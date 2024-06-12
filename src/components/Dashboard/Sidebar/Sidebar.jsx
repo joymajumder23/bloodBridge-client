0ce362508eda85469f59ad8ffa47ddd04051c737
joymajumder23 <joymajumderjishu@gmail.com>
2024-06-12 21:55:58 +0600

    react leaflet map added to contact us page and some updated

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -251,14 +251,14 @@ const Sidebar = () => {
 
             <span className='mx-4 font-medium'>Home</span>
           </NavLink>
-          <button
+          {/* <button
             onClick={logOut}
             className='flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300   hover:text-gray-700 transition-colors duration-300 transform'
           >
             <GrLogout className='w-5 h-5' />
 
             <span className='mx-4 font-medium'>Logout</span>
-          </button>
+          </button> */}
         </div>
       </div>
     </>
