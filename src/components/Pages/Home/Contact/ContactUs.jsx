fc1a3a89779f1c2a70f47e3aaf16adc125c64f41
joymajumder23 <joymajumderjishu@gmail.com>
2024-06-12 21:51:15 +0600

    react leaflet map added to contact us page

--- src/components/Pages/Home/Contact/ContactUs.jsx
+++ src/components/Pages/Home/Contact/ContactUs.jsx
@@ -2,26 +2,28 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
 const ContactUs = () => {
+    const position = [22.3568944, 91.7899716];
     return (
         <div className="max-w-screen-xl mx-auto mt-24">
-            <h1 className="text-4xl text-center">Contact Us</h1>
-            <div className="lg:flex gap-4">
-                <div className="lg:w-1/2 p-2">
+            <h1 className="text-4xl text-center font-bold">Contact Us</h1>
+            <div className="lg:flex items-center gap-4">
+                <div className="lg:w-1/2 p-2 mt-6">
                     <form className="space-y-4" action="">
                         <input type="text" placeholder="Name" className="input input-bordered w-full rounded-none" /> <br />
                         <input type="text" placeholder="Email" className="input input-bordered w-full rounded-none" /> <br />
                         <input type="text" placeholder="Subject" className="input input-bordered w-full rounded-none" /> <br />
                         <textarea className="textarea textarea-bordered w-full rounded-none" placeholder="Message"></textarea>
+                        <input className='btn bg-red-500 text-white rounded-none' type="submit" value="Send" />
                     </form>
                 </div>
                 <div className="lg:w-1/2 p-2">
-                    <h1>Our Location</h1>
-                    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <h1 className='text-xl font-semibold'>Our Location</h1>
+                    <MapContainer style={{ height: '50vh', width: '100%' }}  center={position} zoom={13} scrollWheelZoom={false}>
                         <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={[51.505, -0.09]}>
+                        <Marker position={position}>
                             <Popup>
                                 A pretty CSS3 popup. <br /> Easily customizable.
                             </Popup>
--- src/components/Pages/Home/Home/Home.jsx
+++ src/components/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import Blogs from "../Blogs/Blogs";
 import ContactUs from "../Contact/ContactUs";
@@ -6,6 +7,9 @@ import Featured from "../Featured/Featured";
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <Banner></Banner>
             <Featured></Featured>
             <Blogs></Blogs>
